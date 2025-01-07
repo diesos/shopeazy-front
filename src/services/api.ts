f772ae653611e61ccec2f4c7d5e8f84b4eeb9f85
BenCDA <cardosobenjamin01@gmail.com>
2025-01-07 10:54:04 +0100

    (feat): user api call

--- src/services/api.ts
+++ src/services/api.ts
@@ -31,3 +31,35 @@ export const deleteItem = async (name: string, quantity: number) => {
     });
     return response.data;
   };
+
+  // get user
+  export const getUser = async() => {
+    const response = await apiClient.get("/user/me");
+    return response.data;
+  };
+
+ // login
+ export const login = async (email: string, password: string) => {
+    const response = await apiClient.post("/user/login", {email, password});
+    return response.data;
+ };
+
+ // register
+ export const register = async (name: string, email: string, password: string) => {
+    const response = await apiClient.post("/user/register", {name, email, password});
+    return response.data;
+ };
+
+ // reset password
+ export const resetPassword = async (token: string, newPassword: string) => {
+    const response = await apiClient.post(
+      "/user/reset-password",
+      { newPassword }, 
+      {
+        headers: {
+          Authorization: `Bearer ${token}`, 
+        },
+      }
+    );
+    return response.data; 
+  };
