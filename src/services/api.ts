2f025a113619a56b08e65903fa27da4cf92e3bbe
BenCDA <cardosobenjamin01@gmail.com>
2025-01-07 10:41:25 +0100

    (feat): items api call

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,19 +1,33 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: "http://localhost:9000/api",
+    baseURL: "http://localhost:9000",
     headers: {
         "Content-Type": "application/json",
     },
 });
 // get articles
 export const getItem = async () => {
-    const response = await apiClient.get("/item");
+    const response = await apiClient.get("/list/item");
     return response.data;
 }
 
 // add articles
 export const addItem = async (name: string, quantity: number) => {
-    const response - await apiClient.post("/item", {name, quantity});
+    const response = await apiClient.post("/list/item", {name, quantity});
     return response.data;
 };
+
+// update articles
+export const updateItem = async(name: string, quantity: number) => {
+    const response = await apiClient.put("/list/item", {name, quantity});
+    return response.data;
+};
+
+// delete articles
+export const deleteItem = async (name: string, quantity: number) => {
+    const response = await apiClient.delete(`/list/item`, {
+      params: { name, quantity }, // Les paramètres sont passés ici
+    });
+    return response.data;
+  };
